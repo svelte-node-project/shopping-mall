dc15e9533cf384ad6ecdea8ac98336f2ffc42969
parisa kalaki <parisakalaki@gmail.com>
2024-02-22 11:26:57 +0200

    Changed require to import

--- api/app.js
+++ api/app.js
@@ -1,34 +1,36 @@
-const express = require('express')
-const { Pool } = require('pg');
-const cors = require('cors')
-const app = express()
+import express from "express";
+import pg from "pg";
+import cors from "cors";
 
-app.use(express.json())
-app.use(cors())
+const { Pool } = pg;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 const pool = new Pool({
-    user: 'username',
-    host: 'database',
-    database: 'database',
-    password: 'password',
-    port: 5432,
-  });
+  user: "username",
+  host: "database",
+  database: "database",
+  password: "password",
+  port: 5432,
+});
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
 
 async function getTestData() {
-  const res = await pool.query('SELECT * FROM todos');
-  return res.rows
+  const res = await pool.query("SELECT * FROM todos");
+  return res.rows;
 }
 
-app.get('/api/todos', async (request, response) => {
-  const data = await getTestData()
-  response.json(data)
-})
+app.get("/api/todos", async (request, response) => {
+  const data = await getTestData();
+  response.json(data);
+});
 
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
