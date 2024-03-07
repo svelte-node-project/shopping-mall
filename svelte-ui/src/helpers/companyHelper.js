cc303514f74a43a616a05caa0748356a9b444db2
Olena Barabash <elena.barabash@gmail.com>
2024-03-07 12:20:02 +0200

    Fixed link

--- svelte-ui/src/helpers/companyHelper.js
+++ svelte-ui/src/helpers/companyHelper.js
@@ -3,7 +3,7 @@ const getCompanyLink = async (id) => {
         `http://localhost:3001/companies/${id}?main_category=true`
     );
     const companyInfo = await data.json();
-    return `en/stores/${companyInfo.category_name_in_url}/${companyInfo.name_in_url}`;
+    return `/en/stores/${companyInfo.category_name_in_url}/${companyInfo.name_in_url}`;
 };
 
 export { getCompanyLink };
